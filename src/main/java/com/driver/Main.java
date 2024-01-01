b312e6a93a0c696c01de4b19ff876c896bbfe9a1
Suraj Singh <mesinghzr@gmail.com>
2024-01-01 21:55:47 +0530

    M

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B objB = new B();
+        String result=objB.meth();
+        System.out.println(result);
+    }
 }
